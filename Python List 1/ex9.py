76196a960a745cc0ddc00f70c9c554fb41f1edce
Rafael Oliveira <patojosefos24fe@gmail.com>
2025-08-14 22:41:31 -0300

    Update ex9.py

--- Python List 1/ex9.py
+++ Python List 1/ex9.py
@@ -1,3 +1,5 @@
+#Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o total de dias.
+
 cigarros_por_dia = int(input("Quantos cigarros você fuma por dia: "))
 anos_fumando = int(input("Há quantos anos você fuma: "))
 
@@ -6,3 +8,4 @@ minutos_perdidos = total_cigarros * 10
 dias_perdidos = minutos_perdidos / (60 * 24)
 
 print(f"Você perdeu aproximadamente {dias_perdidos:.0f} dias de vida.")
+
